6a50064ff95c0e632028170351c519614f9ef952
qunzman <manuel.kunz@gewa.ch>
2026-03-13 11:37:35 +0100

    Sälü hoi sälü

--- jump_n_run/main.py
+++ jump_n_run/main.py
@@ -1,4 +1,7 @@
 import pygame
+import Player
+import plates
+
 
 class Game:
     def __init__(self):
@@ -6,6 +9,14 @@ class Game:
         self.window_widht = 800
         self.window_height = 400
         self.window = pygame.display.set_mode((self.window_widht, self.window_height))
+        self.playerimage = pygame.image.load("Robot/Robot Standart.png")
+        self.playerjumpimage = pygame.image.load("Robot/Jump.png")
+        self.player = Player.Player(self, 350, 100, 100, 170, self.playerimage, self.playerjumpimage)
+        self.background = pygame.image.load("Background/Hintergrund.png")
+        self.background = pygame.transform.scale(self.background, (self.window_widht, self.window_height))
+        self.klone = []
+        self.plateimage = pygame.image.load("Plattform/Plattform.png")  # dein Platten-Bild
+        self.game_map = plates.Plate(self, 128, 128, self.plateimage)
         self.run()
 
     def run(self):
@@ -15,13 +26,15 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
             
-            self.window.fill((137, 207, 240))
+            self.window.blit(self.background, (0, 0))
+            self.player.update()
+ 
+            neuer_klon = self.game_map.klon(self.klone, 100)
+            if neuer_klon:
+                self.klone.append(neuer_klon)
 
-
-
-
-
-
+            for klon in self.klone:
+                klon.update()
 
             pygame.display.update()
 
